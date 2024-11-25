242d414a579bf00c70157aea5a2897f7d6922cdc
Tjorven Grawunder <tjorven.grawunder@gmail.com>
2024-11-25 19:25:27 +0100

    Bottle is now food like an apple

--- src/main/java/de/tjorven/lidlmod/item/LidlBottle.java
+++ src/main/java/de/tjorven/lidlmod/item/LidlBottle.java
@@ -1,9 +1,15 @@
 package de.tjorven.lidlmod.item;
 
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.BottleItem;
+import net.minecraft.world.item.Item;
 
-public class LidlBottle extends BottleItem {
-    public LidlBottle(Properties pProperties) {
-        super(pProperties);
+public class LidlBottle extends Item {
+
+    public LidlBottle(Properties properties) {
+        super(properties.food(Foods.APPLE));
     }
+
+
+
 }
